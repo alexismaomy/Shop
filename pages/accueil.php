70527304ba0236c4c4742761d767ac67839e7735
alexis.maomy <alexis.maomy@yahoo.fr>
2014-10-03 12:12:13 +0200

    Ajout rubrique news sur accueil

--- pages/accueil.php
+++ pages/accueil.php
@@ -4,6 +4,9 @@
     <p>Quantité: 20 Prix moyen par pièce: 50 </p>
     <h3>Vente</h3>
     <p>Quantité: 15 Prix moyen par pièce: 55 </p>
+    <h2>News</h2> 
+    <p>Le site progresse bien</p> 
+ 
 </div>
 
 
